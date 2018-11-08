3ed50151491f03ee3e0cd02bb6da58a80387212e
Danilo Valério da Silva <danilo_vs@hotmail.com>
2018-11-07 22:14:45 -0200

    Projeto spring mongodb criado

---
+++ src/main/java/com/danilovalerio/projmongo/ProjmongoApplication.java
@@ -0,0 +1,12 @@
+package com.danilovalerio.projmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjmongoApplication.class, args);
+	}
+}
